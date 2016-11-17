00f270c686e389824dd380fafc7d31893c406c4c
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-11-17 13:57:20 -0600

    finished constructor and variables some

---
+++ Pizza-master/pizza.js
@@ -0,0 +1,35 @@
+class pizza{
+  constructor(s,mt,vt){
+    this.size = s;
+    this.meatToppings = mt;
+    this.veggieToppings = vt;
+  }
+sizeCost(){
+  if(this.size[0]){
+    return = 7.99;
+}
+sizeCost(){
+  if(this.size[1]){
+    return = 9.99;
+}
+sizeCost(){
+  if(this.size[2]){
+    return = 12.99;
+}
+sizeCost(){
+  if(this.size[3]){
+    return = 15.99;
+}
+toppingCost(top){
+  if(top == "meat")
+    return = (*0.99)
+topp
+
+
+}
+
+
+  //type class functions below
+
+
+}
