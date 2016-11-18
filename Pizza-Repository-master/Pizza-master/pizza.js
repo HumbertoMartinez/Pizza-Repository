40408cb15bc6c6c5c11a3f38715de8dc153f27c2
HumbertoMartinez <9hmartinez@stpatrick.org>
2016-11-18 13:46:55 -0600

    pizza program done

---
+++ Pizza-Repository-master/Pizza-master/pizza.js
@@ -0,0 +1,44 @@
+class pizza{
+  constructor(s,mt,vt){
+    this.size = s;
+    this.meatToppings = mt;
+    this.veggieToppings = vt;
+  }
+sizeCost(){
+  if(this.size[0]){
+    return = 7.99;
+}
+sizeCost(){
+  if(this.size[1]){
+    return = 9.99;
+}
+sizeCost(){
+  if(this.size[2]){
+    return = 12.99;
+}
+sizeCost(){
+  if(this.size[3]){
+    return = 15.99;
+}
+toppingCost(top){
+  if(top == "meat")
+    return this.meatToppings*0.99;
+}
+  if(top=="veggie"){
+    return this.veggieToppings*0.50;
+     }
+  }
+  price(){
+    return this.sizeCost+this.toppingCost("meat","veggie");
+  }
+promotionalDeal(pizza, percent){
+    this.pizza-("percent");
+    return (1-20%/100+.8*value);
+
+}
+
+
+  //type class functions below
+
+
+}
